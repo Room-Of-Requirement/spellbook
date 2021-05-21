db76150aa056f6aecd772d3006ab30e7f6221139
Jack Maloney <jmmaloney4@gmail.com>
2021-05-21 14:55:13 -0500

    Updates to Taskfile

--- script/apply.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kubectl apply --kustomize=./cluster/base/flux-system
-kubectl apply --kustomize=./cluster/base/flux-system
--- script/teardown.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mpssh -u k3s -f ./inventory.toml -- "/usr/local/bin/k3s-uninstall.sh"
-mpssh -u k3s -f ./inventory.toml -- "/usr/local/bin/k3s-agent-uninstall.sh"
