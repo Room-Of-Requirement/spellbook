5ed4980b502bbeac65f3f1cb68b1ae780bee8653
Jack Maloney <jmmaloney4@gmail.com>
2021-05-25 22:37:17 -0500

    Work setting up nodes

---
+++ script/ceph-prep.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+host="$1"
+shift
+
+for DISK in "$@"; do 
+    ssh $host -- sudo sgdisk --zap-all "$DISK"
+    ssh $host -- sudo dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync
+done
+
+ssh $host -- sh -c 'ls /dev/mapper/ceph-* | xargs -I% -- sudo dmsetup remove %'
+ssh $host -- sudo rm -rf /dev/ceph-*
+ssh $host -- sudo rm -rf /dev/mapper/ceph-*
+ssh $host -- sudo rm -rf /dev/dm-*
