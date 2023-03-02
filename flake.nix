feec8873887f8d2c71c956eedf612f9498ece05a
Jack Maloney <jack@maloney.ai>
2023-03-02 00:18:40 -0600

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+    outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; config.allowUnsupportedSystem = true; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            kubectl
+            kubernetes-helm
+            ansible
+          ];
+        };
+      });
+}
